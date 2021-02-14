887a575d4942e0458aedaf80b007a9784eccb81a
Vic Sweeting <vic.sweeting@curi.com>
2021-02-14 14:15:38 -0500

    On branch main  Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   ADPhoneList.ps1
    	new file:   CreateNewUser (1).ps1
    	new file:   CreateNewUser.ps1
    	deleted:    readme.txt
    	deleted:    test.txt
    	deleted:    test2.txt
    	deleted:    test3.txt

---
+++ ADPhoneList.ps1
@@ -0,0 +1,8 @@
+﻿Get-AdUser -Filter * -Properties OfficePhone, DisplayName |`
+Select-Object DisplayName, `
+@{Name = "OfficePhone";Expression = {($_.OfficePhone -replace '[^0-9]')}} |`
+Export-Csv -notypeinformation -Path $env:USERPROFILE\Desktop\phonelist.csv
+
+Import-Csv -Path "$env:USERPROFILE\Desktop\phonelist.csv" | Where-Object { $_.PSObject.Properties.Value -ne '' } | Export-Csv -Path "$env:USERPROFILE\Desktop\phonelist_clean.csv" -NoTypeInformation
+
+Remove-Item $env:USERPROFILE\Desktop\phonelist.csv
